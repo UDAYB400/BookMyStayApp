9951cbc3a37faed0ed4bfe6fc63836199ba5032a
UDAYB400 <udaykumar33322@gmail.com>
2026-03-11 15:08:26 +0530

    Use Case 1: Application Entry & Welcome Message

--- src/UseCaseHotelBookingApp.java
+++ src/UseCaseHotelBookingApp.java
@@ -2,7 +2,6 @@ public class UseCaseHotelBookingApp {
     public static void main(String[] args) {
         System.out.println("Welcome to Hotel Booking Managment System  ");
 
-
         System.out.println("System intialized sucessfully");
     }
 
