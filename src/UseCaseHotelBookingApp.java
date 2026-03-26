4b40aab1acfe85217161a867e8bdabf8c3d00cff
UDAYB400 <udaykumar33322@gmail.com>
2026-03-26 14:23:12 +0530

    UseCase

--- src/UseCaseHotelBookingApp.java
+++ src/UseCaseHotelBookingApp.java
@@ -1,9 +1,58 @@
-public class UseCaseHotelBookingApp {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Hotel Booking Managment System  ");
 
-        System.out.println("System intialized sucessfully");
+abstract class Room {
+    String type;
+    int beds;
+    double price;
+
+    Room(String type, int beds, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.price = price;
     }
 
+    void display() {
+        System.out.println(type + " | Beds: " + beds + " | Price: " + price);
+    }
+}
 
+class SingleRoom extends Room {
+    SingleRoom() {
+        super("Single Room", 1, 1000);
+    }
+}
+
+class DoubleRoom extends Room {
+    DoubleRoom() {
+        super("Double Room", 2, 2000);
+    }
+}
+
+class SuiteRoom extends Room {
+    SuiteRoom() {
+        super("Suite Room", 3, 5000);
+    }
+}
+
+public class UseCaseHotelBookingApp {
+    public static void main(String[] args) {
+
+        Room r1 = new SingleRoom();
+        Room r2 = new DoubleRoom();
+        Room r3 = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        System.out.println("Hotel Booking System v2.0");
+
+        r1.display();
+        System.out.println("Available: " + singleAvailable);
+
+        r2.display();
+        System.out.println("Available: " + doubleAvailable);
+
+        r3.display();
+        System.out.println("Available: " + suiteAvailable);
+    }
 }
